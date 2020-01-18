04b5f4cd91cd8475bcf452e4f07a2fc1c4eb6629
Victor Nitu <nitu.v.r@gmail.com>
2020-01-18 13:09:32 +0100

    first commit

---
+++ api.go
@@ -0,0 +1,92 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"time"
+)
+
+type (
+	API struct {
+		Client        *http.Client
+		baseUrl       string
+		Authorization string
+	}
+	Status struct {
+		Code int `json:"code"`
+		Message string `json:"message"`
+	}
+)
+
+func (status Status) IsError() bool {
+	return status.Code >= 400
+}
+
+func New(baseUrl string) API {
+	return API{&http.Client{Timeout: 10 * time.Second}, baseUrl, ""}
+}
+
+func (api *API) Get(url string, target interface{}) (status Status, err error) {
+	return api.request("GET", url, nil, target)
+}
+
+func (api *API) Head(url string, target interface{}) (status Status, err error) {
+	return api.request("GET", url, nil, target)
+}
+
+func (api *API) Post(url string, body interface{}, target interface{}) (status Status, err error) {
+	return api.push("POST", url, body, target)
+}
+
+func (api *API) Put(url string, body interface{}, target interface{}) (status Status, err error) {
+	return api.push("PUT", url, body, target)
+}
+
+func (api *API) Patch(url string, body interface{}, target interface{}) (status Status, err error) {
+	return api.push("PATCH", url, body, target)
+}
+
+func (api *API) push(method string, url string, body interface{}, target interface{}) (status Status, err error) {
+	b, err := json.Marshal(body)
+	if err != nil {
+		return
+	}
+	return api.request(method, url, bytes.NewBuffer(b), target)
+}
+
+func (api *API) request(method string, url string, body io.Reader, target interface{}) (status Status, err error) {
+	req, err := http.NewRequest(method, api.baseUrl+url, body)
+	if err != nil {
+		return
+	}
+	req.Header.Add("Authorization", api.Authorization)
+	req.Header.Set("Content-Type", "application/json")
+	res, err := api.Client.Do(req)
+	if err != nil {
+		return
+	}
+	status = Status{res.StatusCode, ""}
+	switch status.Code {
+	case 400:
+		status.Message = "external service: bad request"
+		return
+	case 401:
+		status.Message = "external service: unauthorized"
+		return
+	case 403:
+		status.Message = "external service: forbidden"
+		return
+	case 404:
+		status.Message = "external service: not found"
+		return
+	case 500:
+		status.Message = "external service: unavailable"
+		return
+	}
+	err = json.NewDecoder(res.Body).Decode(target)
+	return
+}
+
+
